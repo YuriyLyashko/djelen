54df547f48c28de729f70136a9a41d089ed91e09
YuriyLyashko <yu.i.lyashko@gmail.com>
2016-11-20 21:01:13 +0200

    create mysql connecton

--- djelen/djelen/settings.py
+++ djelen/djelen/settings.py
@@ -1,3 +1,4 @@
+from os.path import join
 """
 Django settings for djelen project.
 
@@ -77,8 +78,8 @@ WSGI_APPLICATION = 'djelen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'read_default_file': join(BASE_DIR, 'djelen', 'my.cnf')},
     }
 }
 
