c196af5d05d34bde85300d36c983eb30e7335666
YuriyLyashko <yu.i.lyashko@gmail.com>
2016-11-21 12:23:14 +0200

    create models and add to admin site

--- djelen/calculation/admin.py
+++ djelen/calculation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ElectrifiedObject, ElectricityMeter, Readings
 
 # Register your models here.
+admin.site.register(ElectrifiedObject)
+admin.site.register(ElectricityMeter)
+admin.site.register(Readings)
--- djelen/calculation/models.py
+++ djelen/calculation/models.py
@@ -1,4 +1,32 @@
 from django.db import models
 
 # Create your models here.
-#class
+class ElectrifiedObject(models.Model):
+    name = models.CharField("Назва електрифікованого об'єкту", max_length=255)
+    address = models.CharField("Адреса електрифікованого об'єкту", max_length=255, null=True)
+
+    def __str__(self):
+        return "Назва електрифікованого об'єкту: " + str(self.name)
+
+class ElectricityMeter(models.Model):
+    number = models.PositiveIntegerField("Номер лічильника")
+    el_object = models.ForeignKey(ElectrifiedObject)
+
+    def __str__(self):
+        return "Номер лічильника: " + str(self.number)
+
+class Readings(models.Model):
+    previous_readings = models.PositiveIntegerField("Попередні покази лічильника")
+    current_readings = models.PositiveIntegerField("Поточні покази лічильника")
+    electricity_meter = models.ForeignKey(ElectricityMeter)
+
+    def __str__(self):
+        return "Покази лічильника: {}, {}".format(self.previous_readings, self.current_readings)
+
+class Tariffs(models.Model):
+    tarrif_limit_1 = models.FloatField()
+    tarrif_limit_2 = models.FloatField()
+    tarrif_limit_3 = models.FloatField()
+
+    def __str__(self):
+        return "Тарифи: {}, {}, {}".format(self.tarrif_limit_1, self.tarrif_limit_2, self.tarrif_limit_3)
